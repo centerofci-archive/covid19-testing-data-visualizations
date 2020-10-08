677fa9f94c888b380558be4bad02967bb447cf10
Alexander James Phillips <ajp.global@gmail.com>
2020-10-08 08:57:19 +0100

    Include other viral material types in EUA data points and remove unsupported LoD units

--- src/data.js
+++ src/data.js
@@ -6,7 +6,9 @@ import { test_name_FDA_ref_panel_to_EUA_mapping } from "./data_FDA_test_name_map
 function filter_FDA_LoD (data)
 {
   return data
+    // Remove row of headers
     .slice(1)
+    // Remove any that lack LoD data
     .filter(row => !!row[3])
 }
 
@@ -25,7 +27,7 @@ const processed_FDA_reference_panel_LoD_data = transform_FDA_LoD(filter_FDA_LoD(
 
 //
 
-const FDA_EUAs_data_by_name = FDA_EUAs_data.reduce((accum, test) =>
+const FDA_EUAs_data_by_id = FDA_EUAs_data.reduce((accum, test) =>
   {
     if (accum[test.test_name])
     {
@@ -35,31 +37,48 @@ const FDA_EUAs_data_by_name = FDA_EUAs_data.reduce((accum, test) =>
     accum[test.test_name] = test
     return accum
   }, {})
+// window.FDA_EUAs_data_by_id = FDA_EUAs_data_by_id
 
 //
 
 const combined_data = processed_FDA_reference_panel_LoD_data.map(d => {
-  let test_EUA_data = FDA_EUAs_data_by_name[d.test_name]
+  let test_EUA_data = FDA_EUAs_data_by_id[d.test_name]
 
   if (!test_EUA_data)
   {
     const mapped_name = test_name_FDA_ref_panel_to_EUA_mapping[d.test_name]
-    test_EUA_data = FDA_EUAs_data_by_name[mapped_name]
+    test_EUA_data = FDA_EUAs_data_by_id[mapped_name]
   }
 
   if (!test_EUA_data)
   {
-    console.error(`No test named: "${d.test_name}" from "${d.developer_name}" in FDA_EUAs_data_by_name (window.FDA_EUAs_data_by_name)`)
-    return false
+    console.error(`No test named: "${d.test_name}" from "${d.developer_name}" in FDA_EUAs_data_by_id (window.FDA_EUAs_data_by_id)`)
   }
 
+  return { test_EUA_data, d }
+})
+.filter(({ test_EUA_data }) => !!test_EUA_data)
+.map(({ test_EUA_data, d }) => {
   // Note:  * 1000 to do conversion from per uL to per mL
   const own_LoD_min = test_EUA_data.lod_min * 1000
   const own_LoD_max = test_EUA_data.lod_max * 1000
+  const own_LoD_units = test_EUA_data.lod_units
 
   const x = Math.log10(d.LoD)
   const y_min = Math.log10(own_LoD_min)
   const y_max = Math.log10(own_LoD_max)
+
+  const synthetic_specimen__viral_material = test_EUA_data.synthetic_specimen__viral_material
+  const naked_RNA = synthetic_specimen__viral_material.includes("Naked RNA")
+  const synthetic_viral_particles = synthetic_specimen__viral_material.includes("Synthetic Viral Particles")
+  const live_virus = synthetic_specimen__viral_material.includes("Live Virus")
+  const inactivation_methods = new Set([
+    "Inactivated Virus (Chemical)",
+    "Inactivated Virus (Gamma radiation)",
+    "Inactivated Virus (Heat)",
+    "Inactivated Virus (method unspecified)",
+  ])
+  const inactivated_virus = !!synthetic_specimen__viral_material.filter(x => inactivation_methods.has(x)).length
 
   return {
     developer_name: d.developer_name,
@@ -68,24 +87,53 @@ const combined_data = processed_FDA_reference_panel_LoD_data.map(d => {
     ref_panel_LoD: d.LoD,
     own_LoD_min,
     own_LoD_max,
-    own_LoD_units: test_EUA_data.lod_units,
+    own_LoD_units,
     min_max_diff: own_LoD_min !== own_LoD_max,
     x,
     y_min,
     y_max,
     sample_media_type: d.sample_media_type,
-    synthetic_specimen__viral_material: test_EUA_data.synthetic_specimen__viral_material,
-    naked_RNA: test_EUA_data.synthetic_specimen__viral_material.includes("Naked RNA"),
+    synthetic_specimen__viral_material,
+    naked_RNA,
+    synthetic_viral_particles,
+    live_virus,
+    inactivated_virus,
   }
 })
-.filter(d => !!d)
+
+
+const supported_own_LoD_units = new Set([
+  "genome copies / μL",
+  "PFU / μL",
+])
+const unsupported_own_LoD_units = new Set([
+  "genome copies / reaction",
+  "TCID50 / mL",
+  "TCID50 / reaction",
+  "",
+])
+const filtered_combined_data = combined_data
+.filter(data => {
+  const include = supported_own_LoD_units.has(data.own_LoD_units)
+
+  if (!include && !unsupported_own_LoD_units.has(data.own_LoD_units))
+  {
+    console.error(`unhandled own_LoD_units: ${data.own_LoD_units}`)
+  }
+
+  return include
+})
+
+
+console.log(`Combined data count: ${filtered_combined_data.length} out of ${FDA_reference_panel_LoD_data.length} FDA reference panel points and ${FDA_EUAs_data.length} FDA EUA extracted data. ${combined_data.length - filtered_combined_data.length} removed due to unsupported own LoD units.`)
+
 
 let min_x = Number.MAX_SAFE_INTEGER
 let max_x = Number.MIN_SAFE_INTEGER
 let min_y = Number.MAX_SAFE_INTEGER
 let max_y = Number.MIN_SAFE_INTEGER
 
-combined_data.forEach(d => {
+filtered_combined_data.forEach(d => {
   min_x = Math.min(d.x, min_x)
   max_x = Math.max(d.x, max_x)
   min_y = Math.min(d.y_min, min_y)
@@ -98,7 +146,7 @@ const min_y_tidy = Math.floor(min_y)
 const max_y_tidy = Math.ceil(max_y)
 
 export {
-  combined_data,
+  filtered_combined_data,
   min_x_tidy,
   max_x_tidy,
   min_y_tidy,
