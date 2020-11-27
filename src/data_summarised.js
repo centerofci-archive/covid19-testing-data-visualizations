9825d8f8d56e15edb1fcb195e561f7bffa228502
Alexander James Phillips <ajp.global@gmail.com>
2020-11-27 19:17:31 +0000

    Update summarised EUA data

--- src/data_summarised.js
+++ src/data_summarised.js
@@ -9,8 +9,8 @@ export const summarised_data = {
     "have_parsed": 62,
     "not_parsed": 82,
     "explicitly_specified": 4,
-    "reference_available": 17,
-    "not_specified": 41
+    "reference_available": 18,
+    "not_specified": 40
   },
   "primer_probe_sequences_in_top_10_test_EUAs": {
     "explicitly_specified": 0,
@@ -45,73 +45,87 @@ export const summarised_data = {
     "not_parsed": 2,
     "antigens": {
       "count": 0,
-      "example_refs": []
+      "example_ref": null
     },
     "inactivated_virus_chemical": {
       "count": 0,
-      "example_refs": []
+      "example_ref": null
     },
     "inactivated_virus_gamma_radiation": {
       "count": 4,
-      "example_refs": [
-        "114?h=4",
-        "39?h=2",
-        "96?h=2"
-      ]
+      "example_ref": "58?h=4"
     },
     "inactivated_virus_heat": {
       "count": 13,
-      "example_refs": [
-        "79?h=3",
-        "6?h=1",
-        "83?h=6"
-      ]
+      "example_ref": "49?h=3"
     },
     "inactivated_virus_method_unspecified": {
       "count": 6,
-      "example_refs": [
-        "97?h=1",
-        "136?h=3",
-        "121?h=2"
-      ]
+      "example_ref": "132?h=4"
     },
     "live_virus": {
-      "count": 17,
-      "example_refs": [
-        "140?h=7",
-        "43?h=8",
-        "22?h=0"
-      ]
+      "count": 18,
+      "example_ref": "53?h=4"
     },
     "naked_rna": {
       "count": 77,
-      "example_refs": [
-        "134?h=0",
-        "77?h=7",
-        "29?h=1"
-      ]
+      "example_ref": "84?h=3"
     },
     "partial_live_virus": {
       "count": 1,
-      "example_refs": [
-        "63?h=3"
-      ]
+      "example_ref": "63?h=3"
     },
     "synthetic_viral_particles": {
       "count": 21,
-      "example_refs": [
-        "85?h=1",
-        "3?h=2",
-        "93?h=3"
-      ]
+      "example_ref": "62?h=4"
     },
     "not_specified": {
-      "count": 3,
-      "example_refs": [
-        "82?h=12",
-        "276?h=1",
-        "90?h=1"
-      ]
+      "count": 2,
+      "example_ref": "90?h=1"
+    }
+  },
+  "lod_viral_material_top_10_tests_weighted": {
+    "have_parsed": 9,
+    "not_parsed": 0,
+    "antigens": {
+      "count": 0,
+      "example_ref": null
+    },
+    "inactivated_virus_chemical": {
+      "count": 0,
+      "example_ref": null
+    },
+    "inactivated_virus_gamma_radiation": {
+      "count": 0,
+      "example_ref": null
+    },
+    "inactivated_virus_heat": {
+      "count": 0,
+      "example_ref": null
+    },
+    "inactivated_virus_method_unspecified": {
+      "count": 18,
+      "example_ref": "74?h=3"
+    },
+    "live_virus": {
+      "count": 23,
+      "example_ref": "22?h=0"
+    },
+    "naked_rna": {
+      "count": 13,
+      "example_ref": "29?h=1"
+    },
+    "partial_live_virus": {
+      "count": 0,
+      "example_ref": null
+    },
+    "synthetic_viral_particles": {
+      "count": 15,
+      "example_ref": "18?h=10"
+    },
+    "not_specified": {
+      "count": 0,
+      "example_ref": null
     }
   }
 }
@@ -135,6 +149,9 @@ function generate_data_for_display (categories, begin_offset = 0)
 
     const percentage = `${(ratio * 100).toFixed(1)}%`
     const label_w_percentage = label + `  (${percentage})`
+    const refs_as_urls = refs
+      .filter(ref => !!ref)
+      .map(ref => "https://anot8.org/1772.2/" + ref)
 
     const data = {
       label,
@@ -143,7 +160,7 @@ function generate_data_for_display (categories, begin_offset = 0)
       ratio,
       begin: running_total + begin_offset,
       color,
-      refs: refs.map(ref => "https://anot8.org/1772.2/" + ref),
+      refs: refs_as_urls,
     }
 
     running_total += ratio
@@ -287,49 +304,49 @@ export const lod_viral_material = generate_data_for_display([
   {
     label: "Live Virus",
     value: summarised_data.lod_viral_material.live_virus.count,
-    refs: summarised_data.lod_viral_material.live_virus.example_refs,
+    refs: [summarised_data.lod_viral_material.live_virus.example_ref],
     color: COLORS.steel_blue,
   },
   {
     label: "Partial Live Virus",
     value: summarised_data.lod_viral_material.partial_live_virus.count,
-    refs: summarised_data.lod_viral_material.partial_live_virus.example_refs,
+    refs: [summarised_data.lod_viral_material.partial_live_virus.example_ref],
     color: COLORS.light_steel_blue,
   },
   {
     label: "Synthetic Viral Particles",
     value: summarised_data.lod_viral_material.synthetic_viral_particles.count,
-    refs: summarised_data.lod_viral_material.synthetic_viral_particles.example_refs,
+    refs: [summarised_data.lod_viral_material.synthetic_viral_particles.example_ref],
     color: COLORS.sky_blue,
   },
   {
     label: "Inactivated Virus (Gamma Radiation)",
     value: summarised_data.lod_viral_material.inactivated_virus_gamma_radiation.count,
-    refs: summarised_data.lod_viral_material.inactivated_virus_gamma_radiation.example_refs,
+    refs: [summarised_data.lod_viral_material.inactivated_virus_gamma_radiation.example_ref],
     color: COLORS.pale_turquoise,
   },
   {
     label: "Inactivated Virus (Heat)",
     value: summarised_data.lod_viral_material.inactivated_virus_heat.count,
-    refs: summarised_data.lod_viral_material.inactivated_virus_heat.example_refs,
+    refs: [summarised_data.lod_viral_material.inactivated_virus_heat.example_ref],
     color: COLORS.powder_blue,
   },
   {
     label: "Inactivated Virus (Method Unspecified)",
     value: summarised_data.lod_viral_material.inactivated_virus_method_unspecified.count,
-    refs: summarised_data.lod_viral_material.inactivated_virus_method_unspecified.example_refs,
+    refs: [summarised_data.lod_viral_material.inactivated_virus_method_unspecified.example_ref],
     color: COLORS.vanilla_ice,
   },
   {
     label: "Naked RNA",
     value: summarised_data.lod_viral_material.naked_rna.count,
-    refs: summarised_data.lod_viral_material.naked_rna.example_refs,
+    refs: [summarised_data.lod_viral_material.naked_rna.example_ref],
     color: COLORS.thistle,
   },
   {
     label: "Not Specified",
     value: summarised_data.lod_viral_material.not_specified.count,
-    refs: summarised_data.lod_viral_material.not_specified.example_refs,
+    refs: [summarised_data.lod_viral_material.not_specified.example_ref],
     color: COLORS.rosy_brown,
   },
 ], 0.25)
